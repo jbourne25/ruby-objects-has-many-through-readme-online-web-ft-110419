8d24aa4fb1df0b33ee9018a82323615107d910d6
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-22 20:03:18 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,29 @@
 class Customer
   
+  #@@all = []
+  
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals 
+    #grab all the meals, then find the meals that were orderd by this customer  
+    Meal.all.find_all{ |meal| meal.customer == self }
+  end
+  
+  def waiters 
+    #grabs all the customer's meals, then returns all the waiters that carried these meals  
+    self.meals.collect { |meal| meal.waiter }
+  end 
+  
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
-
+  
+  def initialize(name, years_experience)
+    @name = name 
+    @years_experience = years_experience
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end 
+  
+  def meals 
+    #grab all the meals, then return the waiters that carried out these meals 
+    Meal.all.find_all{ |meal| meal.waiter == self }
+  end 
+  
+  def best_tipper
+    #go through the meals, find the customer that left the largest tip 
+    meals.Customer.find{ |customer| customer.tip }.largest 
+    
+  end 
 end
